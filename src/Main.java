1357702a80cf3042c91cedacb5b908adc1a6371a
juandavidmoya <148277709+juandavidmoya@users.noreply.github.com>
2024-07-23 16:25:11 -0500

    calculadora1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    private static java.util.Scanner Scanner;
+
+    public static void main(String[] args) {
         System.out.println("bienvenido a tu cañlculadora");
+        Scanner entrada = Scanner;
+        System.out.println("ingrese el primer numero ");
+        int num1 = 0;
+        Scanner.nextInt();
+
+        System.out.println("ingrese el segundo numero");
+        int num2=Scanner.nextInt();
+
+
     }
 }
